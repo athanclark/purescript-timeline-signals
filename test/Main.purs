b6cee8977725ac37c0a19f1ae442a2c87740aec2
Athan Clark <athan.clark@gmail.com>
2020-08-09 09:46:32 -0400

    less warnings

--- test/Main.purs
+++ test/Main.purs
@@ -1,32 +1,32 @@
 module Test.Main where
 
-import Timeline.Signals.Mappings (Mappings (..))
-import Timeline.Signals.Mappings.TimeSpaces (addTimeSpaceForceScoped)
-import Timeline.Signals.Mappings.Timelines (addTimelineScoped)
-import Timeline.Signals.Mappings.Events (addEventScoped)
-import Timeline.Signals.Mappings.TimeSpans (addTimeSpanScoped)
-import Timeline.UI.TimeSpace (TimeSpace (..))
-import Timeline.UI.Timeline (Timeline)
-import Timeline.UI.Event (Event)
-import Timeline.UI.TimeSpan (TimeSpan)
+-- import Timeline.Signals.Mappings (Mappings (..))
+-- import Timeline.Signals.Mappings.TimeSpaces (addTimeSpaceForceScoped)
+-- import Timeline.Signals.Mappings.Timelines (addTimelineScoped)
+-- import Timeline.Signals.Mappings.Events (addEventScoped)
+-- import Timeline.Signals.Mappings.TimeSpans (addTimeSpanScoped)
+-- import Timeline.UI.TimeSpace (TimeSpace (..))
+-- import Timeline.UI.Timeline (Timeline)
+-- import Timeline.UI.Event (Event)
+-- import Timeline.UI.TimeSpan (TimeSpan)
 
 import Prelude
-import Data.Maybe (Maybe (..))
+-- import Data.Maybe (Maybe (..))
 import Data.Identity (Identity)
-import Data.NonEmpty (NonEmpty (..))
-import Data.Traversable (traverse_)
+-- import Data.NonEmpty (NonEmpty (..))
+-- import Data.Traversable (traverse_)
 import Effect (Effect)
 import Effect.Aff (Aff, launchAff_)
-import Effect.Ref (new) as Ref
-import Effect.Unsafe (unsafePerformEffect)
-import Zeta.Types (writeOnly) as Z
-import IxZeta.Map (new) as IxSignalMap
-import Unsafe.Coerce (unsafeCoerce)
-import Test.Spec (describe, it, SpecT)
+-- import Effect.Ref (new) as Ref
+-- import Effect.Unsafe (unsafePerformEffect)
+-- import Zeta.Types (writeOnly) as Z
+-- import IxZeta.Map (new) as IxSignalMap
+-- import Unsafe.Coerce (unsafeCoerce)
+import Test.Spec (describe, SpecT)
 import Test.Spec.Runner (runSpec)
 import Test.Spec.Reporter.Console (consoleReporter)
-import Test.QuickCheck (arbitrary)
-import Test.QuickCheck.Gen (Gen)
+-- import Test.QuickCheck (arbitrary)
+-- import Test.QuickCheck.Gen (Gen)
 
 main :: Effect Unit
 main = launchAff_ $ runSpec [consoleReporter] tests
